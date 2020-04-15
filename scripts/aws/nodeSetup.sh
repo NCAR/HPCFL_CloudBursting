4071b2b63db3bba5b626143709c7470511bd8d6a
Will Shanks <shanks@ucar.edu>
2020-04-15 20:16:53 +0000

    Closes #9, Switched compute nodes to centos7

--- scripts/aws/nodeSetup.sh
+++ scripts/aws/nodeSetup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 10
 tries=0
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2saltInstall.sh" ec2-user@$1:~/
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2saltInstall.sh" centos@$1:~/
 ret=$?
 while [ $ret -ne 0 -a $tries -lt 20 ]; do
   sleep 20
-  scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2saltInstall.sh" ec2-user@$1:~/
+  scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2saltInstall.sh" centos@$1:~/
   ret=$?
   tries=$((tries+=1))
   #echo $tries
@@ -15,9 +15,10 @@ if [ $ret -ne 0 ]; then
   exit 7
 fi
 
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2salt-minion.service" ec2-user@$1:~/
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2ifcfg-eth0" ec2-user@$1:~/
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2minion" ec2-user@$1:~/
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2keys/$1.pem" ec2-user@$1:~/minion.pem
-scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2keys/$1.pub" ec2-user@$1:~/minion.pub
-ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 ec2-user@$1 "./saltInstall.sh $1"
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2salt-minion.service" centos@$1:~/
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2ifcfg-eth0" centos@$1:~/
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2minion" centos@$1:~/
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2keys/$1.pem" centos@$1:~/minion.pem
+scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 "$2keys/$1.pub" centos@$1:~/minion.pub
+ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 centos@$1 "chmod +x saltInstall.sh"
+ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i/home/slurm/.ssh/hpcfl2 centos@$1 "./saltInstall.sh $1"
--- scripts/aws/saltInstall.sh
+++ scripts/aws/saltInstall.sh
@@ -7,20 +7,23 @@ sleep 2
 sudo ip r add default via 192.168.2.10
 
 printf "\n192.168.0.120 salt\n" | sudo sh -c "cat >> /etc/hosts"
-sudo yum install https://repo.saltstack.com/py3/amazon/salt-py3-amzn2-repo-latest.amzn2.noarch.rpm -y
-sudo amazon-linux-extras install epel -y
+sudo yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm -y
+#sudo yum install https://repo.saltstack.com/py3/amazon/salt-py3-amzn2-repo-latest.amzn2.noarch.rpm -y
+sudo yum install epel-release -y
+#sudo amazon-linux-extras install epel -y
 sudo yum clean expire-cache
 sudo yum update -y
 sudo yum install salt-minion --disablerepo=epel -y
-sudo mv -f /home/ec2-user/salt-minion.service /usr/lib/systemd/system/salt-minion.service
-sudo mv -f /home/ec2-user/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo mv -f /home/ec2-user/minion.pem /etc/salt/pki/minion/minion.pem
+sudo mv -f /usr/lib/systemd/system/salt-minion.service /home/centos/oldsalt-minion.service
+sudo mv -f /home/centos/salt-minion.service /usr/lib/systemd/system/salt-minion.service
+sudo mv -f /home/centos/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0
+sudo mv -f /home/centos/minion.pem /etc/salt/pki/minion/minion.pem
 sudo chown root:root /etc/salt/pki/minion/minion.pem
 sudo chmod 400 /etc/salt/pki/minion/minion.pem
-sudo mv -f /home/ec2-user/minion.pub /etc/salt/pki/minion/minion.pub
+sudo mv -f /home/centos/minion.pub /etc/salt/pki/minion/minion.pub
 sudo chown root:root /etc/salt/pki/minion/minion.pem
 sudo chmod 644 /etc/salt/pki/minion/minion.pub
-sudo mv -f /home/ec2-user/minion /etc/salt/minion
+sudo mv -f /home/centos/minion /etc/salt/minion
 sudo systemctl daemon-reload
 sudo systemctl enable salt-minion
 sudo reboot
